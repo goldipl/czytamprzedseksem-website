99cbfb2e128f8054c82e41b56cdbe5e3a2909231
goldipl <mgodlewski92@gmail.com>
2024-08-08 18:14:35 +0200

    fix config file

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-const path = require('path');
-
+const path = require('path')
+ 
 module.exports = {
-  // Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  trailingSlash: true,
-
-  // Specify asset prefix for correct path handling in the exported version
-  assetPrefix: '/',
-
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-};
+}
