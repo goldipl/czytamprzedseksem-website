3b800dc48b812ca0adf881969b8c3ac6c56979cf
goldipl <mgodlewski92@gmail.com>
2024-08-15 19:51:07 +0200

    add document file

---
+++ src/pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="pl">
+        <Head>
+          {/* <link rel="icon" href="./icons/favicon.ico" />
+          <link rel="apple-touch-icon" href="./icons/favicon.ico" /> */}
+          <title>Czytam przed seksem - Hubert Pajączkowski</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
