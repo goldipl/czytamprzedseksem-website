14a0a7cd9d772041535ef05f54e1e349a9f88822
goldipl <mgodlewski92@gmail.com>
2024-08-15 20:39:24 +0200

    comment meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Czytam przed seksem - Hubert Pajączkowski",
-  keywords:
-    "książki, literatura, inspiracje, czytanie, seks, przed seksem, instagram, edukator seksualny, czytam przed seksem, pozytywna seksualność, przemiany ról płciowych, różne modele związków, trendy, dzieciństwo i dojrzewanie, Hubert Pajączkowski",
-  description:
-    "Odwiedź oficjalną stronę 'Czytam Przed Seksem'. Znajdziesz tu inspiracje literackie na wyjątkowe chwile. Edukator seksualny - zajrzyj na nasz Instagram!",
+  // title: "Czytam przed seksem - Hubert Pajączkowski",
+  // keywords:
+  //   "książki, literatura, inspiracje, czytanie, seks, przed seksem, instagram, edukator seksualny, czytam przed seksem, pozytywna seksualność, przemiany ról płciowych, różne modele związków, trendy, dzieciństwo i dojrzewanie, Hubert Pajączkowski",
+  // description:
+  //   "Odwiedź oficjalną stronę 'Czytam Przed Seksem'. Znajdziesz tu inspiracje literackie na wyjątkowe chwile. Edukator seksualny - zajrzyj na nasz Instagram!",
 };
 
 export default function RootLayout({
