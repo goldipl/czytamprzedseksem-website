e2ec41f30dedd5e26833c3d8eb990560754df083
goldipl <mgodlewski92@gmail.com>
2024-08-16 15:48:08 +0200

    styling and handle 2nd lvl menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import logo from "./../../public/icons/header/logo.svg";
 import menuDownIcon from "./../../public/icons/header/menu-down-icon.svg";
 
 const Header = () => {
+  const [activeDropdown, setActiveDropdown] = useState(null);
+
+  const handleDropdownToggle = (dropdown: any) => {
+    setActiveDropdown((prev) => (prev === dropdown ? null : dropdown));
+  };
+
   return (
     <div className="header">
       <div className="header-wrapper">
@@ -18,20 +24,49 @@ const Header = () => {
           </Link>
         </div>
         <nav className="header-wrapper__nav">
-          <ul>
+          <ul className="nav-first-lvl">
             <li className="nav-expand">
-              <span>
-                Warsztaty{" "}
+              <span onClick={() => handleDropdownToggle("firstDropdown")}>
+                Warsztaty
                 <img
                   src={menuDownIcon.src}
                   alt="strzałka"
-                  height={12}
-                  width={20}
+                  height={9}
+                  width={16}
                 />
               </span>
+              <ul
+                className={`nav-second-lvl ${
+                  activeDropdown === "firstDropdown" ? "active" : ""
+                }`}
+              >
+                <li>
+                  <Link href="">Szkoła podstawowa</Link>
+                </li>
+                <li>
+                  <Link href="">Szkoła średnia</Link>
+                </li>
+              </ul>
             </li>
-            <li>
-              <Link href="">Szkolenia</Link>
+            <li className="nav-expand">
+              <span onClick={() => handleDropdownToggle("secondDropdown")}>
+                Szkolenia
+                <img
+                  src={menuDownIcon.src}
+                  alt="strzałka"
+                  height={9}
+                  width={16}
+                />
+              </span>
+              <ul
+                className={`nav-second-lvl ${
+                  activeDropdown === "secondDropdown" ? "active" : ""
+                }`}
+              >
+                <li>
+                  <Link href="">Z rodzicami i kadrą pedagogiczną</Link>
+                </li>
+              </ul>
             </li>
             <li>
               <Link href="">Konsultacje</Link>
