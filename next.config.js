e473eb455cf8fab6909a3bb0d6f45bb065fa4a5c
goldipl <mgodlewski92@gmail.com>
2024-08-16 18:16:45 +0200

    fill & styling Hero section with video and upgrade config for video ads

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
-const path = require('path')
- 
+const path = require('path');
+
 module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[hash].[ext]',
+          outputPath: 'static/media',
+        },
+      },
+    });
+
+    return config;
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-}
+};
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import heroVideo from "./../../public/movie/background-movie.mp4";
+import avatar from "./../../public/img/avatar.jpg";
 
 const Hero = () => {
   return (
     <div className="hero" id="hero">
-      <h1>Hero</h1>
+      <video src={heroVideo} autoPlay muted loop className="hero-video">
+        Your browser does not support the video tag.
+      </video>
+      <div className="hero-wrapper">
+        <img src={avatar.src} alt="avatar" className="hero-wrapper__avatar" />
+        <div className="hero-wrapper__text">
+          <h1>Czytam przed seksem</h1>
+          <h3>
+            <span>-</span> Hubert Pajączkowski <span>-</span>
+          </h3>
+          <p>| Edukator seksualny |</p>
+        </div>
+      </div>
     </div>
   );
 };
