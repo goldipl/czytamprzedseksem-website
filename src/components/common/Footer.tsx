05e101894b186248bc1406fa06a01336ce9ee28c
goldipl <mgodlewski92@gmail.com>
2024-08-17 18:33:53 +0200

    add footer with footer content and styles

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="footer">
       <div className="footer-wrapper">
-        <p>Footer 2024</p>
+        <p>Czytam przed seksem - Hubert Pajączkowski © {currentYear}</p>
       </div>
     </div>
   );
