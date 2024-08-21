7407ec3a01c79559332e9c961894bc159bc51112
goldipl <mgodlewski92@gmail.com>
2024-08-21 20:38:42 +0200

    change hero content, h1, h2 styles

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -13,14 +13,15 @@ const Hero = () => {
       <div className="hero-wrapper">
         <Image src={avatar} alt="avatar" className="hero-wrapper__avatar" />
         <div className="hero-wrapper__text">
-          <h1>Czytam przed seksem</h1>
-          <h3>
-            <span>-</span> Hubert Pajączkowski <span>-</span>
-          </h3>
+          <h1>Hubert Pajączkowski</h1>
+          <h2>
+            <span>-</span> edukator seksualny <span>-</span>
+          </h2>
           <div className="hero-desc">
-            <p>| Edukator seksualny | pozytywna seksualność |</p>
-            <p>| przemiany ról płciowych | różne modele związków | trendy |</p>
-            <p>| dzieciństwo i dojrzewanie |</p>
+            <p>
+              Współpracuję ze szkołami, organizacjami pozarządowymi, ośrodkami
+              opiekuńczo-wychowawczymi i z klientami indywidualnymi.
+            </p>
           </div>
           <Link href="#news-section" className="button01">
             Sprawdź najnowsze treści
