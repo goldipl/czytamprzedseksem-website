d6a1d1ad638a77bff67c3f5d1d2e0c72270c01c9
goldipl <mgodlewski92@gmail.com>
2024-09-03 14:26:06 +0200

    add nofollow

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,7 +21,7 @@ class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <meta
+          {/* <meta
             name="description"
             content="Hubert Pajączkowski - edukator seksualny, współpraca ze szkołami, organizacjami pozarządowymi, i klientami indywidualnymi."
           />
@@ -51,7 +51,11 @@ class MyDocument extends Document {
             content="Edukator seksualny współpracujący ze szkołami, organizacjami i klientami indywidualnymi."
           />
           <meta name="twitter:image" content="/img/hubert/hubert02.jpg" />
-          <meta name="robots" content="index, follow" />
+          <meta name="robots" content="index, follow" /> */}
+
+          {/*Github version noindex, no follow */}
+          <meta name="robots" content="noindex, nofollow" />
+
           <link rel="icon" href="./icons/favicon.ico" />
           <link rel="apple-touch-icon" href="./icons/favicon.ico" />
           <title>Czytam przed seksem - Hubert Pajączkowski</title>
