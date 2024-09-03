088f23a3974391f3526da27c14953b7be6c145e8
goldipl <mgodlewski92@gmail.com>
2024-09-03 13:49:57 +0200

    add seo meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,13 +16,12 @@ class MyDocument extends Document {
     return (
       <Html lang="pl">
         <Head>
-          <meta name="robots" content="noindex, nofollow" />
           <meta charSet="UTF-8" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          {/* <meta
+          <meta
             name="description"
             content="Hubert Pajączkowski - edukator seksualny, współpraca ze szkołami, organizacjami pozarządowymi, i klientami indywidualnymi."
           />
@@ -52,7 +51,7 @@ class MyDocument extends Document {
             content="Edukator seksualny współpracujący ze szkołami, organizacjami i klientami indywidualnymi."
           />
           <meta name="twitter:image" content="/img/hubert/hubert02.jpg" />
-          <meta name="robots" content="index, follow" /> */}
+          <meta name="robots" content="index, follow" />
           <link rel="icon" href="./icons/favicon.ico" />
           <link rel="apple-touch-icon" href="./icons/favicon.ico" />
           <title>Czytam przed seksem - Hubert Pajączkowski</title>
